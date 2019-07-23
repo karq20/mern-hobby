d350f3947545a42b311f88c53c4b0882e3116dd3
Mohit Sudhakar <mohit.sudhakar@gmail.com>
2019-07-23 15:04:09 +0530

    updated mongodb uri

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://admin:admin@cluster0-7crxd.mongodb.net/hobby?retryWrites=true&w=majority'
+  mongoURI: 'mongodb+srv://admin:admin@cluster0-f3acq.mongodb.net/test?retryWrites=true&w=majority'
 }
